479027ada44814f3425b46b151632b32ef5caec9
Farooq <30216781+mianfarooqazam@users.noreply.github.com>
2024-11-03 17:20:09 +0500

    Services Improved

--- src/components/Craft/Index.jsx
+++ src/components/Craft/Index.jsx
@@ -25,11 +25,11 @@ function Craft() {
         ".card",
         {
           y: 600,
-          scale: 0.9,
+          opacity: 0,
         },
         {
           y: 0,
-          scale: 1.1,
+          opacity: 1,
           duration: 0.5,
           ease: Power4,
           transformOrigin: "bottom 50% -50",
@@ -40,30 +40,30 @@ function Craft() {
 
   return (
     <div
-      data-color="cyan"
+      data-color="light"
       className="craft section w-full sm:flex gap-x-40 justify-between 
           items-center px-8 py-8 sm:px-10 relative "
     >
       <div className="ltext sm:sticky sm:top-[10%] left-0 sm:w-1/2 ">
+        <h1 className="texthead text-[5vh] leading-[6vh] sm:text-[9.8vh] sm:leading-[12vh] mt-10 mb-10 text-black">
+          BERC Services
+        </h1>
         <p
-          className="ptag font-[Sansita] text-[2.6vh] sm:text-[2.9vh] 
-                font-medium leading-[4.4vh] sm:leading-[4.2vh] "
+          className="ptag text-[2.6vh] sm:text-[2.9vh] 
+                font-medium leading-[4.4vh] sm:leading-[4.2vh] text-black"
         >
           BERC offers a comprehensive suite of services tailored to your unique
           needs. From consultation to implementation, our dedicated team strives
           for excellence at every step, contributing to the creation of
           energy-efficient, sustainable, and resilient buildings nationwide.
         </p>
-        <h1 className="texthead font-[SansitaReg] text-[5vh] leading-[6vh] sm:text-[9.8vh] sm:leading-[12vh] mt-10 mb-10">
-          Sustainable Building Solutions Aligned with SDGs & ECBC
-        </h1>
 
         {/* button */}
-        <Button bgColor="bg-none" text="OUR SOLUTIONS" />
+        {/* <Button bgColor="bg-none" text="OUR SOLUTIONS" /> */}
       </div>
       <div
         ref={container}
-        className="right cards sm:w-1/2 flex items-center justify-center"
+        className="right cards sm:w-1/2 flex flex-wrap items-center justify-center gap-10"
       >
         <Card />
       </div>
